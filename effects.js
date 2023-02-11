f28ba914112f4019db6ec01240593fcf1ab83145
Sakana Kochiya <93559155+SakanaKochiya@users.noreply.github.com>
2023-02-11 16:06:39 +0700

    Add effects pt.2

---
+++ effects.js
@@ -0,0 +1,44 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+
+/* if you want to always shows the FANCY AF effect
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+
+*/
+
+/*
+if not
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+*/
