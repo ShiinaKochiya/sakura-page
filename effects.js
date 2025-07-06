54ae8eab258858d5e23eb02c5f0ecc777ba4d875
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-07-06 09:58:21 +0700

    external pj upd

--- effects.js
+++ effects.js
@@ -48,4 +48,9 @@ document.addEventListener('DOMContentLoaded', generateMasonryGallery);
 
 //If you question that "Wait, did I really made this", then yes, I made it once with React and Tailwind, I just rewrote a lazier version for this one
 //The react one is here https://dreamchasers.akvns.org/retro/the-show-must-go-on
-// Due to NDA contract-related reasons, I cannot share the exact source-code of the web until the project is official announced
+//Due to NDA contract-related reasons, I cannot share the exact source-code of the web until the project is official announced
+//On second thought, since the webpage is annouce before the result date, here's the source code of the gallery I made with React and Tailwind:
+//https://github.com/arknights-vns/the-web/blob/main/components/ImageGallery.tsx and https://github.com/arknights-vns/the-web/blob/main/app/retro/the-show-must-go-on/page.tsx
+//The ImageGallery components fetch all images in specified folder (for this instance, @/public/retro/the-show-must-go-on/album) 
+//Then render the gallery in /app/retro/the-show-must-go-on/page.tsx
+//It will give the gallery of images I shoot during Arknights Vietnam Station Offline: [The Show Must Go On] Offline event at the end of the retro page
